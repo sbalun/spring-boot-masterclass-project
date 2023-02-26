bffb349d13a6e2bba16a4ffe7a934187d48eb968
Scott Balun <sbalun@gmail.com>
2023-02-26 12:20:14 -0500

    add dependency injection @Component to CustomerService

--- src/main/java/com/balun/springboot/masterclass/customer/CustomerController.java
+++ src/main/java/com/balun/springboot/masterclass/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.balun.springboot.masterclass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class CustomerController {
 
     private CustomerService customerService;
 
+    @Autowired
     public CustomerController(){
         customerService = new CustomerService();
     }
--- src/main/java/com/balun/springboot/masterclass/customer/CustomerService.java
+++ src/main/java/com/balun/springboot/masterclass/customer/CustomerService.java
@@ -1,5 +1,8 @@
 package com.balun.springboot.masterclass.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
     Customer getCustomer(){
         return new Customer(1L, "Roland Deschain");
