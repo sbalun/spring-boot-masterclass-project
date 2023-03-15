46c57b225833d02ebdee1821e52a2c15c289d23e
Scott Balun <sbalun@gmail.com>
2023-03-14 21:53:32 -0400

    refactor @Component annotations and update them to use the more specific annotations for controller, repository, and service

--- src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryDB.java
+++ src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryDB.java
@@ -2,12 +2,13 @@ package com.balun.springboot.masterclass.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
+@Repository
 @Primary
 public class CustomerRepositoryDB implements CustomerRepository {
     @Override
--- src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryMock.java
+++ src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryMock.java
@@ -1,11 +1,12 @@
 package com.balun.springboot.masterclass.customer;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Component(value = "mock")
+@Repository(value = "mock")
 public class CustomerRepositoryMock implements CustomerRepository {
     @Override
     public List<Customer> getCustomers(){
