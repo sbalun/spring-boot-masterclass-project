1a847f34b265c960204a9418228858446111eac1
Scott Balun <sbalun@gmail.com>
2023-03-16 22:47:03 -0400

    add password to customer object and required edits to support it

--- src/main/java/com/balun/springboot/masterclass/customer/Customer.java
+++ src/main/java/com/balun/springboot/masterclass/customer/Customer.java
@@ -3,10 +3,16 @@ package com.balun.springboot.masterclass.customer;
 public class Customer {
     private final Long id;
     private final String name;
+    private final String password;
 
-    Customer(Long id, String name) {
+    Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public Long getId() {
@@ -15,5 +21,14 @@ public class Customer {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
--- src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryMock.java
+++ src/main/java/com/balun/springboot/masterclass/customer/CustomerRepositoryMock.java
@@ -11,11 +11,11 @@ public class CustomerRepositoryMock implements CustomerRepository {
     @Override
     public List<Customer> getCustomers(){
         return Arrays.asList(
-            new Customer(1L, "Roland Deschain"),
-            new Customer(2L, "Eddie Dean"),
-            new Customer(3L, "Jake Chambers"),
-            new Customer(4L, "Susannah Dean"),
-            new Customer(5L, "Oy")
+            new Customer(1L, "Roland Deschain", "abc123"),
+            new Customer(2L, "Eddie Dean", "def123"),
+            new Customer(3L, "Jake Chambers", "hij123"),
+            new Customer(4L, "Susannah Dean", "lmn123"),
+            new Customer(5L, "Oy", "opq123")
 
         );
     }
