55d45d80cb8145dfa7bb675552fa8fb2bf844a1e
Scott Balun <sbalun@gmail.com>
2023-05-02 21:25:46 -0400

    Finish work on custom exceptions

--- src/main/java/com/balun/springboot/masterclass/exception/ApiException.java
+++ src/main/java/com/balun/springboot/masterclass/exception/ApiException.java
@@ -1,16 +1,18 @@
 package com.balun.springboot.masterclass.exception;
-
 import org.springframework.http.HttpStatus;
-
 import java.time.ZonedDateTime;
 
+// This is the class of what the client will see
 public class ApiException {
     private final String message;
     private final Throwable throwable;
     private final HttpStatus httpStatus;
     private final ZonedDateTime zonedDateTime;
 
-    public ApiException(String message, Throwable throwable, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+    public ApiException(String message,
+                        Throwable throwable,
+                        HttpStatus httpStatus,
+                        ZonedDateTime zonedDateTime) {
         this.message = message;
         this.throwable = throwable;
         this.httpStatus = httpStatus;
